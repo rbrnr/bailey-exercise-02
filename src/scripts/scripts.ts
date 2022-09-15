b4810fa915fb7b388035346949e45ad1deb1f079
Ryan Brenner <ryan@rbrnr.com>
2022-09-15 19:16:24 -0400

    Starter lines

--- src/scripts/scripts.ts
+++ src/scripts/scripts.ts
@@ -4,6 +4,23 @@ import imagesLoaded from 'imagesloaded';
 (() => {
   window.onload = function() {
     // write code inside of here. this is a self contained wrapper that runs on page load.
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(el => {
+        if (el.isIntersecting) {
+          
+        }
+      });
+    }, { threshold: .15 });
     
+    gsap.set(document.body, { opacity: 0 });
+
+    imagesLoaded(document.body, () => {
+      gsap.to(document.body, { opacity: 1, duration: 1 });
+
+      // const timeline = gsap.timeline();
+      // timeline.to(document.body, { opacity: 1, duration: 1 });
+    });
   }
 })();
