439dd1a16fc858846e154441a6895b9a1c229409
Bailey <bailey@mentalwildfire.com>
2022-09-21 20:25:39 -0400

    scroll to top and start to Observer

--- src/scripts/scripts.ts
+++ src/scripts/scripts.ts
@@ -6,21 +6,34 @@ import imagesLoaded from 'imagesloaded';
     // write code inside of here. this is a self contained wrapper that runs on page load.
 
     // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(el => {
-        if (el.isIntersecting) {
-          
+
+    const collection = document.querySelector('.collection');
+    const scrollToTopBtn = document.querySelector(".scroll-to-top");
+    const singles = document.querySelectorAll(".single");
+    
+    function scrollToTop(entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          scrollToTopBtn.classList.add("showScrollButton");
+        } else {
+          scrollToTopBtn.classList.remove("showScrollButton");
         }
       });
-    }, { threshold: .15 });
-    
-    gsap.set(document.body, { opacity: 0 });
+    }
+
+    let observer = new IntersectionObserver(scrollToTop);
+    observer.observe(collection);
+
+
+    gsap.set(document.querySelectorAll(".titles"), { opacity: 0 });
+    gsap.set(document.querySelector(".grid"), { opacity: 0 });
+
 
     imagesLoaded(document.body, () => {
-      gsap.to(document.body, { opacity: 1, duration: 1 });
-
-      // const timeline = gsap.timeline();
-      // timeline.to(document.body, { opacity: 1, duration: 1 });
+    
+      const timeline = gsap.timeline();
+      timeline.to(document.querySelectorAll(".titles"), { opacity: 1, duration: 2, stagger: 0.3});
+      gsap.to(document.querySelector(".grid"), { opacity: 1, delay: 2.5 });
     });
   }
 })();
