8b1013f36bf5672b35044f4680775f66ef3023b7
Bailey <bailey@mentalwildfire.com>
2022-09-21 23:09:59 -0400

    added base colors and sticky

--- src/scripts/scripts.ts
+++ src/scripts/scripts.ts
@@ -11,7 +11,7 @@ import imagesLoaded from 'imagesloaded';
     const scrollToTopBtn = document.querySelector(".scroll-to-top");
     const singles = document.querySelectorAll(".single");
     
-    function scrollToTop(entries, observer) {
+    function callback(entries, observer) {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           scrollToTopBtn.classList.add("showScrollButton");
@@ -21,18 +21,122 @@ import imagesLoaded from 'imagesloaded';
       });
     }
 
-    let observer = new IntersectionObserver(scrollToTop);
+    let observer = new IntersectionObserver(callback);
     observer.observe(collection);
 
+    let io = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          modify(entry.target);
+        } else {
+          revert(entry.target);
+        }
+      })
+    }, {
+      threshold: 0.8
+    })
+    
+    function modify(el) {
+      if (el.id === "snorlax") {
+          document.body.style.backgroundColor = 'grey';   
+          document.body.style.transition = '1s';    
+      }
+      if (el.id === "squirtle") {
+        document.body.style.backgroundColor = 'blue';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "jigglypuff") {
+        document.body.style.backgroundColor = 'purple';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "rowlet") {
+        document.body.style.backgroundColor = 'green';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "munchlax") {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "grookey") {
+        document.body.style.backgroundColor = 'green';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "snom") {
+        document.body.style.backgroundColor = 'blue';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "wobbuffet") {
+        document.body.style.backgroundColor = 'purple';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "chansey") {
+        document.body.style.backgroundColor = 'grey';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "clobbopus") {
+        document.body.style.backgroundColor = 'orange';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "lapras") {
+        document.body.style.backgroundColor = 'blue';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "cubone") {
+        document.body.style.backgroundColor = 'orange';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "mightyena") {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "pikachu") {
+        document.body.style.backgroundColor = 'yellow';
+        document.body.style.transition = '1s';
+      }
+      if (el.id === "arcanine") {
+        document.body.style.backgroundColor = 'red';
+        document.body.style.transition = '1s';
+      }
+    }
+    
+    function revert(el) {
+      if (el.id === "snorlax") {
+        el.style.backgroundColor = 'initial';
+        document.body.style.transition = '1s';  
+      }
+      if (el.id === "squirtle") {
+        el.style.backgroundColor = 'initial';
+        document.body.style.transition = '1s';
+      }
+    }
+    
+    io.observe(document.querySelector('body'));
+    io.observe(document.querySelector('#snorlax'));
+    io.observe(document.querySelector('#squirtle'));
+    io.observe(document.querySelector('#jigglypuff'));
+    io.observe(document.querySelector('#rowlet'));
+    io.observe(document.querySelector('#munchlax'));
+    io.observe(document.querySelector('#grookey'));
+    io.observe(document.querySelector('#snom'));
+    io.observe(document.querySelector('#wobbuffet'));
+    io.observe(document.querySelector('#chansey'));
+    io.observe(document.querySelector('#clobbopus'));
+    io.observe(document.querySelector('#lapras'));
+    io.observe(document.querySelector('#cubone'));
+    io.observe(document.querySelector('#mightyena'));
+    io.observe(document.querySelector('#pikachu'));
+    io.observe(document.querySelector('#arcanine'));
 
     gsap.set(document.querySelectorAll(".titles"), { opacity: 0 });
     gsap.set(document.querySelector(".grid"), { opacity: 0 });
+    gsap.set(document.querySelector(".collection"), { opacity: 0 });
 
 
     imagesLoaded(document.body, () => {
     
       const timeline = gsap.timeline();
       timeline.to(document.querySelectorAll(".titles"), { opacity: 1, duration: 2, stagger: 0.3});
+      gsap.to(document.querySelector(".collection"), { opacity: 1, duration: 2 });
       gsap.to(document.querySelector(".grid"), { opacity: 1, delay: 2.5 });
     });
   }
